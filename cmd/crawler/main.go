c3bc75572a54a612b0bae3eb918c1de2ab730aad
Matt Jadud <matthew.jadud@gsa.gov>
2024-10-22 18:01:15 -0400

    Removing unused example type.

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -4,13 +4,6 @@ import (
 	rq "search.eight/internal/queueing"
 )
 
-type Thing struct {
-	A string `json:"a"`
-	B int    `json:"b"`
-}
-
-func (Thing) Kind() string { return "thingie" }
-
 // The crawler looks for jobs on the crawler queue.
 // It exists to pick up URLs and read them into S3.
 // Then, it queues that page for parsing.
