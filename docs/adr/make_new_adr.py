4254306aa1a937b8de0e9238515b938297206897
Matt Jadud <matthew.jadud@gsa.gov>
2024-10-26 15:44:34 -0400

    ADRs introduced.

---
+++ docs/adr/make_new_adr.py
@@ -0,0 +1,18 @@
+import click
+import os
+import shutil
+
+
+@click.command()
+@click.argument("name")
+def main(name):
+    files = os.listdir(os.curdir)
+    adrs = 0
+    for f in files:
+        if f.endswith("md"):
+            adrs += 1
+    shutil.copyfile("TEMPLATE", f"{adrs:04}-{name}.md")
+
+
+if __name__ in "__main__":
+    main()
