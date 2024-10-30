fa807ca1689c71abb68e4efe98a26193266a409a
Matt Jadud <matthew.jadud@gsa.gov>
2024-10-29 22:34:30 -0400

    After a long experiment...

---
+++ internal/util/string_utilities.go
@@ -0,0 +1,16 @@
+package util
+
+import "strings"
+
+func AtoZOnly(s string) string {
+	var result strings.Builder
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		if ('a' <= b && b <= 'z') ||
+			('A' <= b && b <= 'Z') ||
+			('0' <= b && b <= '9') {
+			result.WriteByte(b)
+		}
+	}
+	return result.String()
+}
