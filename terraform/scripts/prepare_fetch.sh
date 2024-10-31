6f9f6515c1917431dadda9843cc85887a2c6d979
Matt Jadud <matthew.jadud@gsa.gov>
2024-10-31 17:52:11 -0400

    Fix/cleanup.

--- terraform/scripts/prepare_fetch.sh
+++ terraform/scripts/prepare_fetch.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
-
 # Exit if any step fails
 set -e
 
-eval "$(jq -r '@sh "GITREF=\(.gitref)"')"
-
-# Useful for debugging the script. Comment out the eval if running for debugging purposes
-# GITREF="refs/heads/<branch-name>"
-
-# Portable construct so this will work everywhere
-# https://unix.stackexchange.com/a/84980
 TMP_DIR=$(mktemp -d -t 'build_temp.XXX')
 TOP_LEVEL_DIR="${PWD}"
 
@@ -27,24 +19,6 @@ pushd "$TMP_DIR"
     zip -r -o -X "${TOP_LEVEL_DIR}/zips/app.zip" /app > /dev/null
   popd
 popd
-
-
-
-# Grab a copy of the zip file for the specified ref
-# curl -s -L "https://github.com/jadudm/eight/archive/${GITREF}.zip" --output local.zip
-# branch=$(echo "$GITREF" | cut -f3 -d"/")
-
-
-
-
-# Zip up just the FAC-main/ subdirectory for pushing
-# Before zip stage, run [ npm ci --production | npm run build ] in /backend/ to get the compiled assets for the site in /static/compiled/
-# unzip -q -u local.zip \*"eight-$branch/backend/*"\*
-# cd "${tmpdir}/FAC-$branch/backend/" &&
-# npm ci --production --silent &&
-# npm run build > '/dev/null' 2>&1 &&
-# zip -r -o -X "${popdir}/app.zip" ./ > /dev/null
-# zip -q -j -r ${popdir}/app.zip fac-*/backend
 
 # Tell Terraform where to find it
 cat << EOF
